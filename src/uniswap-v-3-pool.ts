9e9ce2b4342f205b5ee3c904d016e8baa9d44df5
rajabinekoo <ali.rajabinekoo@gmail.com>
2025-04-15 12:23:21 +0330

    liquidities and log indexes added for event logs

--- src/uniswap-v-3-pool.ts
+++ src/uniswap-v-3-pool.ts
@@ -1,94 +1,95 @@
 import {
   Burn as BurnEvent,
-  Collect as CollectEvent,
-  CollectProtocol as CollectProtocolEvent,
-  Flash as FlashEvent,
-  IncreaseObservationCardinalityNext as IncreaseObservationCardinalityNextEvent,
-  Initialize as InitializeEvent,
+  Swap as SwapEvent,
   Mint as MintEvent,
+  Flash as FlashEvent,
+  Collect as CollectEvent,
+  Initialize as InitializeEvent,
   SetFeeProtocol as SetFeeProtocolEvent,
-  Swap as SwapEvent
-} from "../generated/UniswapV3Pool/UniswapV3Pool"
+  CollectProtocol as CollectProtocolEvent,
+  IncreaseObservationCardinalityNext as IncreaseObservationCardinalityNextEvent,
+} from "../generated/UniswapV3Pool/UniswapV3Pool";
 import {
   Burn,
-  Collect,
-  CollectProtocol,
-  Flash,
-  IncreaseObservationCardinalityNext,
-  Initialize,
+  Swap,
   Mint,
+  Flash,
+  Collect,
+  Initialize,
   SetFeeProtocol,
-  Swap
-} from "../generated/schema"
+  CollectProtocol,
+  IncreaseObservationCardinalityNext,
+} from "../generated/schema";
 
 export function handleBurn(event: BurnEvent): void {
   let entity = new Burn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount = event.params.amount
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
+  );
+  entity.owner = event.params.owner;
+  entity.tickLower = event.params.tickLower;
+  entity.tickUpper = event.params.tickUpper;
+  entity.liquidity = event.params.amount;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.logIndex = event.logIndex;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCollect(event: CollectEvent): void {
   let entity = new Collect(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.recipient = event.params.recipient
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
+  );
+  entity.owner = event.params.owner;
+  entity.recipient = event.params.recipient;
+  entity.tickLower = event.params.tickLower;
+  entity.tickUpper = event.params.tickUpper;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleCollectProtocol(event: CollectProtocolEvent): void {
   let entity = new CollectProtocol(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
+  );
+  entity.sender = event.params.sender;
+  entity.recipient = event.params.recipient;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleFlash(event: FlashEvent): void {
   let entity = new Flash(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-  entity.paid0 = event.params.paid0
-  entity.paid1 = event.params.paid1
+  );
+  entity.sender = event.params.sender;
+  entity.recipient = event.params.recipient;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
+  entity.paid0 = event.params.paid0;
+  entity.paid1 = event.params.paid1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleIncreaseObservationCardinalityNext(
@@ -96,83 +97,85 @@ export function handleIncreaseObservationCardinalityNext(
 ): void {
   let entity = new IncreaseObservationCardinalityNext(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  );
   entity.observationCardinalityNextOld =
-    event.params.observationCardinalityNextOld
+    event.params.observationCardinalityNextOld;
   entity.observationCardinalityNextNew =
-    event.params.observationCardinalityNextNew
+    event.params.observationCardinalityNextNew;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleInitialize(event: InitializeEvent): void {
   let entity = new Initialize(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sqrtPriceX96 = event.params.sqrtPriceX96
-  entity.tick = event.params.tick
+  );
+  entity.sqrtPriceX96 = event.params.sqrtPriceX96;
+  entity.tick = event.params.tick;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMint(event: MintEvent): void {
   let entity = new Mint(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.owner = event.params.owner
-  entity.tickLower = event.params.tickLower
-  entity.tickUpper = event.params.tickUpper
-  entity.amount = event.params.amount
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
+  );
+  entity.sender = event.params.sender;
+  entity.owner = event.params.owner;
+  entity.tickLower = event.params.tickLower;
+  entity.tickUpper = event.params.tickUpper;
+  entity.liquidity = event.params.amount;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.logIndex = event.logIndex;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSetFeeProtocol(event: SetFeeProtocolEvent): void {
   let entity = new SetFeeProtocol(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.feeProtocol0Old = event.params.feeProtocol0Old
-  entity.feeProtocol1Old = event.params.feeProtocol1Old
-  entity.feeProtocol0New = event.params.feeProtocol0New
-  entity.feeProtocol1New = event.params.feeProtocol1New
+  );
+  entity.feeProtocol0Old = event.params.feeProtocol0Old;
+  entity.feeProtocol1Old = event.params.feeProtocol1Old;
+  entity.feeProtocol0New = event.params.feeProtocol0New;
+  entity.feeProtocol1New = event.params.feeProtocol1New;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSwap(event: SwapEvent): void {
   let entity = new Swap(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-  entity.sqrtPriceX96 = event.params.sqrtPriceX96
-  entity.liquidity = event.params.liquidity
-  entity.tick = event.params.tick
+  );
+  entity.sender = event.params.sender;
+  entity.recipient = event.params.recipient;
+  entity.amount0 = event.params.amount0;
+  entity.amount1 = event.params.amount1;
+  entity.sqrtPriceX96 = event.params.sqrtPriceX96;
+  entity.liquidity = event.params.liquidity;
+  entity.tick = event.params.tick;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.logIndex = event.logIndex;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
